d18004d2d64779859069046e99ef5aafb68dd4b0
mina-y-Khalil <mina.y.khalil90@gmail.com>
2025-09-08 16:09:01 -0700

    Create A New Pattern

---
+++ index.js
@@ -0,0 +1,10 @@
+function nextSequence() {
+    randomNumber = Math.floor(Math.random() * 4);
+}
+
+
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+let randomChosenColour = buttonColours[randomNumber];
+
+let gamePattern = [];
